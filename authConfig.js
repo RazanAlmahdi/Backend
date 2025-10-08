55e35001aaa88c2587dcd51920dfd3066b62217c
Razan <94799877+RazanAlmahdi@users.noreply.github.com>
2025-10-08 16:37:03 +0300

    Create authConfig.js

---
+++ authConfig.js
@@ -0,0 +1,11 @@
+// authConfig.js
+const msalConfig = {
+    auth: {
+        clientId: "1a74786d-93df-4ade-a02d-dc4c461dcd54",
+        clientSecret: "jAc8Q~EJ--N1vjaza4R5xT.UTDxEK5Wsk4Penc_W",
+        tenantId: "13dee87d-0ca8-4cf3-8929-dfafdac1fb07"
+    }
+};
+
+module.exports = msalConfig;
+
